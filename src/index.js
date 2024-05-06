b3244b7e07d328e7a1eb4209cd443ba84c60335d
rrishabh-dev <rrishabh9044@gmail.com>
2024-05-06 12:00:33 +0530

    feat: initialize project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from 'react';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
